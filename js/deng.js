98c57dd39cf5cdbd3696a4eeaa6ca81169ab65d0
watlam <144750763+watlam@users.noreply.github.com>
2024-01-04 17:26:12 +0800

    Create deng.js

---
+++ js/deng.js
@@ -0,0 +1,4 @@
+// 春节灯笼
+document.write('<link href="https://cdn.jsdelivr.net/gh/watlam/bloghelper@latest/css/denglong.css" rel="stylesheet">');
+document.write('<div class="deng-box"><div class="deng"><div class="xian"></div><div class="deng-a"><div class="deng-b"><div class="deng-t">元旦</div></div></div><div class="shui shui-a"><div class="shui-c"></div><div class="shui-b"></div></div></div></div>');
+document.write('<div class="deng-box1"><div class="deng"><div class="xian"></div><div class="deng-a"><div class="deng-b"><div class="deng-t">快乐</div></div></div><div class="shui shui-a"><div class="shui-c"></div><div class="shui-b"></div></div></div></div>');
